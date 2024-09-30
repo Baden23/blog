1cc8c228d66f74bf3454d089773fc84ae187dae6
baden <baden.rushworth@gmail.com>
2024-09-30 20:54:29 +0800

    db migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   has_rich_text :body
+  belobgs_to :category
   
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
